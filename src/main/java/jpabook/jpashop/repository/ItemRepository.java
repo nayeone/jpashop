eb8915fc520b74149689535e21724c498ec29a4f
nayeone <ice1011@naver.com>
2023-03-06 10:14:52 +0900

    섹션5-1

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item){
+        if (item.get)
+    }
+}
